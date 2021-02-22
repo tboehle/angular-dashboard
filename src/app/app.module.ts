6070ffcafc8982ee6bf56fb35160fc48f11309bc
Thorben Böhle <thorben.boehle@sick.de>
2021-02-22 14:00:41 +0100

    Add material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppConfigService } from './shared/services/app-config.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [AppConfigService],
   bootstrap: [AppComponent],
 })
