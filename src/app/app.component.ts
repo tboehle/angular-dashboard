1dd09c0d8190919b4fc7a04a6cb8e3b216b662e8
Thorben Böhle <thorben.boehle@sick.de>
2021-02-22 12:50:34 +0100

    Introduce applicationSate on startUp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,27 @@ import { AppConfigService } from './shared/services/app-config.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  applicationState: 'ok' | 'enoperm' | 'authenticated' | 'startup' | 'error' = 'startup';
 
   constructor(private config: AppConfigService) {}
 
   ngOnInit(): void {
     console.info(
-      "%c" +
-          [
-              "=================================",
-              (this.config.isDebug() ? "DEVELOPMENT" : "PRODUCTION") + "\n",
-              "Name: " + this.config.getName(),
-              "Version: " + this.config.getVersion(),
-              "Date: " + this.config.getDate(),
-              "API: " + this.config.getApi(),
-              "================================="
-          ].join("\n"),
-      "color:#2c5aa0;font-weight:bold"
-  )
-  }
+      '%c' +
+        [
+          '=================================',
+          (this.config.isDebug() ? 'DEVELOPMENT' : 'PRODUCTION') + '\n',
+          'Name: ' + this.config.getName(),
+          'Version: ' + this.config.getVersion(),
+          'Date: ' + this.config.getDate(),
+          'API: ' + this.config.getApi(),
+          '=================================',
+        ].join('\n'),
+      'color:#2c5aa0;font-weight:bold',
+    );
 
-  title = 'angular-dashboard';
+    setTimeout(() => {
+      this.applicationState = 'ok';
+    }, 2000);
+  }
 }
