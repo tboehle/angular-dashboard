eb6637395c433e0d15ed122cf38a60b5433c4bd6
Thorben Böhle <thorben.boehle@sick.de>
2021-02-23 08:48:45 +0100

    Remove angular material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatIconModule } from '@angular/material/icon';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppConfigService } from './shared/services/app-config.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarLinkComponent } from './sidebar/sidebar-link/sidebar-link.component';
 
 @NgModule({
   declarations: [AppComponent, SidebarComponent, SidebarLinkComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatIconModule],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [AppConfigService],
   bootstrap: [AppComponent],
 })
